2de779bdcb190e65ddf228ce76733e154f3324eb
Jared Bebb <j.bebb@live.com>
2019-11-28 15:36:03 -0600

    fixed linker error to invalid file path

--- SigmoidNeuron.cpp
+++ SigmoidNeuron.cpp
@@ -6,7 +6,7 @@ SigmoidNeuron::SigmoidNeuron()
 }
 
 //from hinton neural networks for machine learning on coursera, Week 1 "some simple models of neurons"
-double logisticFunction(std::vector<double>inputs, std::vector<double>weights, double bias) {
+double logisticFunction(const std::vector<double>inputs, const std::vector<double>weights, const double bias) {
 	double z = 0.0;
 	z += bias;
 	for (size_t i = 0; i < inputs.size(); i++) {
@@ -16,7 +16,7 @@ double logisticFunction(std::vector<double>inputs, std::vector<double>weights, d
 }
 
 //derivatives of the logit,z with respect to the inputs and the weights are very simple. z = b+ Sum(xiwi)
-double input_layer(std::vector<double> inputs, std::vector<double> weights, double bias) {
+double input_layer(const std::vector<double> inputs, const std::vector<double> weights, const double bias) {
 	double z = 0.0;
 	for (size_t i = 0; i < inputs.size(); i++)
 		z += inputs[i] * weights[i];
@@ -25,23 +25,23 @@ double input_layer(std::vector<double> inputs, std::vector<double> weights, doub
 
 double hidden_layer() { return 0.0; }
 
-double delta_rule_for_learning_linear(double x, double y, double t, double e) {
+double delta_rule_for_learning_linear(const double x, double y, const double t, const double e) {
 	return e * x * (t - y); //t= target output value, y= actual output value, e = learning rate, x = input value
 }
 
 /*
 derivative of the output with respect to each weight
 */
-double delta_rule_for_learning_sigmoid(double x, double y, double t) {
+double delta_rule_for_learning_sigmoid(const double x, double y, const double t) {
 	return -1 * x*y*(1 - y)*(t - y); //x = input value, y = actual output value, t = target output value
 }
 
 //y=1/1+e^-z
-double SigmoidNeuron::sigmoid(double z) {
+double SigmoidNeuron::sigmoid(const double z) {
 	return 1 / (1 + exp(-z));
 }
 
-double SigmoidNeuron::predict(std::vector<double>input) {
+double SigmoidNeuron::predict(const std::vector<double>input) {
 	W1 = { {0.2,0.3},{-0.4,0.5},{0,1} };//W1 and W2 will be calculated in train(), can delete assignment here
 	W2 = { 2,-1,5 };
 	std::vector<double>hidden_layer{ 0.0,0.0,0.0 };//hidden layer is composed of logistic neuron X * W1
@@ -59,7 +59,7 @@ double SigmoidNeuron::predict(std::vector<double>input) {
 	return prediction;
 }
 
-void SigmoidNeuron::train(std::vector<std::vector<double>> inputs, std::vector<int> labels, int array_dims) {
+void SigmoidNeuron::train(const std::vector<std::vector<double>> inputs, const std::vector<int> labels, const int array_dims) {
 	double prediction = 0.0;
 	//W1.assign(array_dims + 1, 0.0);
 	W1 = { {0.0,0.0},{0.0,0.0},{0.0,0.0} };
--- UnitTestExpletics/unittestsigmoidneuron.cpp
+++ UnitTestExpletics/unittestsigmoidneuron.cpp
@@ -18,7 +18,25 @@ namespace test
 		TEST_METHOD(TestSigmoidPredict)
 		{
 			SigmoidNeuron sn;
-			Assert::AreEqual(1.0, sn.predict({ 2.0,3.0 }));
+			//Assert::AreEqual(1.0, sn.predict({ 2.0,3.0 }));
+			Assert::AreEqual(1.0, sn.predict({ 0.5,0.5 }));
+		}
+		TEST_METHOD(TestSigmoidPredict2)
+		{
+			std::vector<std::vector<double>> input{ {1, 1},{0.6, 0.6},{0.4, 0},{0, 0.4},{0, 0} };
+			std::vector<int> labels{ 1,1,0,0,0 };
+			SigmoidNeuron s;
+			s.train(input,labels, (int)2);
+
+			std::vector<double> test{ 0.0,0.0 };
+			std::vector<double> test1{ 0.3,0.44 };
+			std::vector<double> test2{ 0.7,0.84 };
+			std::vector<double> test3{ 1.0,1.0 };
+
+			Assert::AreEqual(0.0, s.predict(test));
+			Assert::AreEqual(0.0, s.predict(test1));
+			Assert::AreEqual(1.0, s.predict(test2));
+			Assert::AreEqual(1.0, s.predict(test3));
 		}
 	};
 }
